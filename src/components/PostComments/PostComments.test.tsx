9b2ddf8f97a177066a681dcf3ec5ee2f8a8f445e
jhulyamfebr@gmail.com <jhulyambrozini@gmail.com>
2023-08-06 13:43:58 -0300

    fazendo testes

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,33 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import Post from '.';
-import PostComment from '.';
+import PostComments from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComments/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+    it('Deve inserir dois comentarios', ()=> {
+        render(<Post />)
+
+        // primeiro comentario
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'estou fazendo um comentário'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentario'))
+        expect(screen.getByText('estou fazendo um comentário')).toBeInTheDocument()
+        
+        // segundo comentario
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'estou fazendo outro comentário'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentario'))
+        expect(screen.getByText('estou fazendo outro comentário')).toBeInTheDocument()
+    })
+    
 });
+
